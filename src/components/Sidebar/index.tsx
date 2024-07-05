f236291c8dd8f87931b237978a48c882a326d6f1
aachinelli-ensitech <alejandro@ensitech.com>
2024-07-05 16:33:59 -0300

    cambios sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Sidebar.module.scss';
 import Button from '../Button';
-import { FiArrowLeftCircle, FiMenu } from 'react-icons/fi';
+import { FiArrowLeftCircle } from 'react-icons/fi';
 
 interface MenuItem {
   name: string;
@@ -14,37 +14,23 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ menuItems }) => {
-  const [collapsed, setCollapsed] = useState(false);
-
-  const toggleCollapse = () => {
-    setCollapsed(!collapsed);
-  };
 
   return (
-    <div className={`${styles.sidebar} ${collapsed ? styles.collapsed : ''}`}>
+    <div className={`${styles.sidebar}`}>
       <div className={styles.header}>
-        {
-          !collapsed ?
           <div className={styles.logo}>
             <span className={styles.logoPro}>Pro</span><span className={styles.logoVisor}>Visor</span><span className={styles.logoID}>ID</span>
-          </div> : null
-        }
-        <button className={styles.toggleButton} onClick={toggleCollapse}>
-          {collapsed ? 
-            <p>
-              <FiMenu />
-            </p>
-            : 
+          </div>
+        <button className={styles.toggleButton}>
             <p>
               <FiArrowLeftCircle />
             </p>
-          }
         </button>
       </div>
       <ul className={styles.menu}>
         {menuItems.map((item, index) => (
           <li key={index} className={styles.menuItem}>
-            <Button label={collapsed ? '' : item.name} icon={item.icon} />
+            <Button label={item.name} icon={item.icon} />
           </li>
         ))}
       </ul>
